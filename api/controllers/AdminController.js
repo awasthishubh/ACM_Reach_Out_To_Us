66c5c0efe048fae9ecda89a11d4834feab63dc72
Shubham Awasthi <shubham0522@gmail.com>
2018-05-01 02:54:34 +0530

    Admin Controller

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -6,6 +6,90 @@
  */
 
 module.exports = {
-	
+	index: function(req,res) {
+		var jwt = require('jsonwebtoken');
+		var token=jwt.verify(req.cookies['token'],'sh');
+
+		Admin.findOne({id:token['id'], token: token['rand']}, function(err, data) {
+			if(error){
+				return res.json(500,{message:"Something is wrong"})
+			}
+			if(!data){
+				return res.json({msg:"Not autherised. Try login again"})
+			}
+
+			Problem.find({}).exec(function(err, records){
+				if(err){
+					console.log(err);
+					return res.json(500,{err:"Something Went Wrong."});
+				}	
+				return res.json(200,records);
+			})
+
+		})
+
+  		
+	},
+
+	delete : function(req,res) {
+		var jwt = require('jsonwebtoken');
+		var token=jwt.verify(req.cookies['token'],'sh');
+
+		Admin.findOne({id:token['id'], rand: token['rand']}, function(err, data) {
+			if(error){
+				return res.json(500,{message:"Something is wrong"})
+			}
+			if(!data){
+				return res.json({msg:"Not autherised. Try login again"})
+			}
+
+			Problem.delete({id:req.param('id')}).exec(function (err,record) {
+				if(err){
+					console.log(err);
+					return res.json(500,{err:"Something Went Wrong."});
+				}
+				return res.json(200,{msg:"Sucess"})
+			})
+		})  		
+	},
+
+	login : function function_name(argument) {
+		Admin.findOne({usid:req.param('usid')}, function(err, data) {
+			if(error){
+				return res.json(500,{message:"Something is wrong"})
+			}
+			if(!data){
+				return res.json({msg:"Invalid Username"})
+			}
+
+			var bcrypt=require('bcrypt-nodejs');
+			bcrypt.compare(req.param('pass'),data.pass,function(err,rest){
+				if(rest){
+					var randomstring = require("randomstring")
+					rand=randomstring.generate(7)
+					data.rand=rand;
+					data.save();
+					var jwt = require('jsonwebtoken');
+					token=jwt.sign({ id: 'data.id', rand: rand },'sh');
+					res.cookie('token',token);
+				}
+				else{
+					return res.json(200,{err:"Invalid Password."});
+				}
+			});
+		})
+	}
+
+	logout: function (req, res) {
+		var jwt = require('jsonwebtoken');
+		var token=jwt.verify(req.cookies['token'],'sh');
+		Admin.findOne({id:token['id']}, function(err, record) {
+			if(err){
+				return res.json(500,{message:"Something is wrong"});
+			}
+			var randomstring = require("randomstring");
+			record.rand=randomstring.generate(7);
+		})
+	}
 };
 
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,18 +6,18 @@
  */
 module.exports = {
 	index: function(req,res) {
-  		Problem.find({where: { public: true }}, function(err, records){
+  		Problems.find({where: { public: true }}, function(err, records){
 			if(err){
 				console.log(err);
 				return res.json(500,{err:"Something Went Wrong."});
 			}	
 			return res.json(200,records);
-		}
+		})
 	},
 
 	create: function(req,res) {
   		data=req.param('data');	
-  		Problem.create(data,function(err,user){
+  		Problems.create(data,function(err,user){
 			if(err){
 				console.log(err);
 				return res.json(500,{err:"Something Went Wrong."});
@@ -27,8 +27,17 @@ module.exports = {
 	},
 
 	cookie: function(req,res) {
-		res.cookie('cook',true);
-		res.send("sd");
+		//res.
+		// console.log(req.cookies);
+		// res.cookie('cooks',"hbzzhb");
+		// res.send("sdz");
+		// verify a token symmetric - synchronous
+		var jwt = require('jsonwebtoken');
+		var token = jwt.sign({ foo: 'bar' });
+		console.log(token);
+		var decoded = jwt.verify(token);
+		console.log(decoded) // bar
+		
 	}
 };
 
