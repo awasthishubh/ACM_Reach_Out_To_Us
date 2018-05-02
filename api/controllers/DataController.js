7a95da121f8433968f09be01faf336fc6497d8b2
Shubham Awasthi <shubham0522@gmail.com>
2018-05-02 22:08:29 +0530

    Completed Chart Data

--- api/controllers/DataController.js
+++ api/controllers/DataController.js
@@ -6,24 +6,6 @@
  */
 
 module.exports = {
-	bardata: function(req,res) {
-		record={}
-		Problems.find({}, function(err,response){
-	        if(err) return res.json(error);
-	        response.forEach(function (data) {
-	        	//console.log(data.time);
-	        	time=(Math.floor((data.time-345600000)/604800000)*604800000).toString();//same value for same week
-	        	//console.log(time);
-	        	if(!record[time]){
-	        		record[time]=1;
-	        	}
-	        	else record[time]+=1;
-	        	console.log(record);
-	        });
-	        return res.json(record)
-	    });
-	},
-
 	pidata: function (req,res) {
 		Problems.native(function(err, collection) {
 		  if (err) return res.json(err);
@@ -35,7 +17,7 @@ module.exports = {
 		});
 	},
 
-	pdata: function (req,res) {
+	bardata: function (req,res) {
 		Problems.native(function(err, collection) {
 		  if (err) return res.json(err);
 
@@ -52,7 +34,27 @@ module.exports = {
 		    return res.ok(results);
 			});
 		});
-	}	
+	}
+
+	// bardata: function(req,res) {
+	// 	record={}
+	// 	Problems.find({}, function(err,response){
+	//         if(err) return res.json(error);
+	//         response.forEach(function (data) {
+	//         	//console.log(data.time);
+	//         	time=(Math.floor((data.time-345600000)/604800000)*604800000).toString();//same value for same week
+	//         	//console.log(time);
+	//         	if(!record[time]){
+	//         		record[time]=1;
+	//         	}
+	//         	else record[time]+=1;
+	//         	console.log(record);
+	//         });
+	//         return res.json(record)
+	//     });
+	// },
+
+		
 
 
 	
